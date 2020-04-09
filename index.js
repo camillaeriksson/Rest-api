af4e20df323d641aa614fc693b4c982eb3e0f98a
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-04-09 10:27:08 +0200

    Added put

--- index.js
+++ index.js
@@ -28,7 +28,26 @@ app.get("/users/:id", (req, res) => {
 });
 
 app.delete("/users/:id", (req, res) => {
-  users.splice(parseInt(req.params.id - 1), 1);
+  const user = users.find((user) => user.id === parseInt(req.params.id));
+  if (!user) {
+    res.status(404).send("The user was not found");
+  } else {
+    users.splice(users.indexOf(user), 1);
+  }
+  res.send();
+});
+
+app.put("/users/:id", (req, res) => {
+  let updatedUser = req.body;
+  updatedUser.id = parseInt(req.params.id);
+  const userIndex = users.findIndex(
+    (user) => user.id === parseInt(req.params.id)
+  );
+  if (userIndex === -1) {
+    res.status(404).send("The user was not found");
+  } else {
+    users[userIndex] = updatedUser;
+  }
   res.send();
 });
 
