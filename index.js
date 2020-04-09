243c565270c43bf1b7bb82e76c12fa1dde34188d
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-04-09 14:13:42 +0200

    Added json

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require("fs");
 
 app.use(express.json());
 
-const users = [
-  { id: 1, firstname: "Lars", lastname: "Bengtsson", age: "45" },
-  { id: 2, firstname: "Ellen", lastname: "Skoglund", age: "19" },
-  { id: 3, firstname: "Malin", lastname: "Karlsson", age: "28" },
-  { id: 4, firstname: "Bengt", lastname: "Lilja", age: "55" },
-];
-
 app.get("/", (req, res) => res.send("Welcome!"));
 
-app.get("/users", (req, res) => res.send(users));
+app.get("/users", (req, res) => {
+  let userData = fs.readFileSync("users.json");
+  let users = JSON.parse(userData);
+  res.send(users);
+});
 
 app.post("/users", (req, res) => {
+  let userData = fs.readFileSync("users.json");
+  let users = JSON.parse(userData);
   users.push(req.body);
   res.status(201);
   res.send();
+  fs.writeFileSync("users.json", JSON.stringify(users));
 });
 
 app.get("/users/:id", (req, res) => {
+  let userData = fs.readFileSync("users.json");
+  let users = JSON.parse(userData);
   const user = users.find((user) => user.id === parseInt(req.params.id));
   if (!user) res.status(404).send("The user was not found");
   res.send(user);
 });
 
 app.delete("/users/:id", (req, res) => {
+  let userData = fs.readFileSync("users.json");
+  let users = JSON.parse(userData);
   const user = users.find((user) => user.id === parseInt(req.params.id));
   if (!user) {
     res.status(404).send("The user was not found");
@@ -35,9 +40,12 @@ app.delete("/users/:id", (req, res) => {
     users.splice(users.indexOf(user), 1);
   }
   res.send();
+  fs.writeFileSync("users.json", JSON.stringify(users));
 });
 
 app.put("/users/:id", (req, res) => {
+  let userData = fs.readFileSync("users.json");
+  let users = JSON.parse(userData);
   let updatedUser = req.body;
   updatedUser.id = parseInt(req.params.id);
   const userIndex = users.findIndex(
@@ -49,6 +57,7 @@ app.put("/users/:id", (req, res) => {
     users[userIndex] = updatedUser;
   }
   res.send();
+  fs.writeFileSync("users.json", JSON.stringify(users));
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
